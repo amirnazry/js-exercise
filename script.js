5c140a70fe79a68220e1c83124f06a630b449a9f
Amir hossein nazeri <123809170+amirnazry@users.noreply.github.com>
2025-09-20 19:42:22 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
 
-const userInput = document.getElementById("userInput").addEventListener('focus',()=>{
+// const userInput = document.getElementById("userInput").addEventListener('focusin',()=>{
+//     righteye.className = 'righteyeafter'
+//     lefteye.className = 'lefteyeafter'
+// })
+// const userout = document.getElementById("userInput").addEventListener('focusout',()=>{
+//     righteye.className = 'righteye'
+//     lefteye.className = 'lefteye'
+// })
+// const type = document.getElementById("userInput").addEventListener('input',()=>{
+//     righteye.style.left = '+1px'
+//     lefteye.style.left = '111px'
+// })
+// const passInput = document.getElementById("passInput").addEventListener('focus',()=>{
+//     righteye.className = 'righteyeup'
+//     lefteye.className = 'lefteyeup'
+// })
+
+const userInput = document.getElementById('userInput')
+const passInput = document.getElementById('passInput')
+
+userInput.addEventListener('focus',() => {
     righteye.className = 'righteyeafter'
     lefteye.className = 'lefteyeafter'
 })
-const type = document.getElementById("userInput").addEventListener('keypress',()=>{
-    righteye.style.left = '171px'
-    lefteye.style.left = '111px'
+userInput.addEventListener('blur',() => {
+    righteye.className = 'righteye'
+    lefteye.className = 'lefteye'
 })
-const passInput = document.getElementById("passInput").addEventListener('focus',()=>{
+
+
+passInput.addEventListener('focus', ()=>{
     righteye.className = 'righteyeup'
     lefteye.className = 'lefteyeup'
 })
+passInput.addEventListener('blur',() => {
+    righteye.className = 'righteye'
+    lefteye.className = 'lefteye'
+})
 
+
+// userInput.addEventListener('keydown', ()=>{
+//     var righteyenum = 75
+//     // var righteyenum =+ righteyenum+  1
+//     righteye.style.top = `${righteyenum}px`
+//     // console.log(righteyenum);
+//     righteyenum++
+// })
